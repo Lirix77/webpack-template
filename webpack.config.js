e29869532a88c97f7ea46dacb3d4c2ef9b020d3c
Lirix77 <lirix77@yandex.by>
2020-09-03 11:39:48 +0300

    webpack config: babel + sass + postcss

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry:{
+        app: './src/index.js'
+    },
+    output:{
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
+        },
+        {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader, 
+                'css-loader',
+                'postcss-loader'
+            ]
+        },
+        {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader, 
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devServer:{
+        overlay: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: '[name].css'
+        })
+    ]
+}
