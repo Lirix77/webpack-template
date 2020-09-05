9707f7dfad7062916e9b0e8a19083348cd3c04fe
Lirix77 <lirix77@yandex.by>
2020-09-06 00:07:29 +0300

    Added plugins for organizing files

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -1,5 +1,7 @@
 const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const PATHS = {
     src: path.join(__dirname, './src'),
@@ -27,6 +29,13 @@ module.exports = {
                 exclude: '/node_modules/'
             },
             {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                },
+            },
+            {
                 test: /\.css$/,
                 use: [
                     'style-loader',
@@ -48,6 +57,23 @@ module.exports = {
     plugins: [
         new MiniCssExtractPlugin({
             filename: `${PATHS.assets}css/[name].css`
-        })
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+              {
+                from: `${PATHS.src}/img`,
+                to: `${PATHS.assets}img`
+              },
+              {
+                from: `${PATHS.src}/static`,
+                to: ''
+              }
+            ]
+          }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            template: `${PATHS.src}/index.html`,
+            filename: 'index.html',
+          })
     ]
 }
