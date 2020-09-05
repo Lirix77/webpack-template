0770dc8d6fab3116b089500df22e113d133f39e0
Lirix77 <lirix77@yandex.by>
2020-09-05 23:35:30 +0300

    add const PATHS

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -1,44 +1,53 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const PATHS = {
+    src: path.join(__dirname, './src'),
+    dist: path.join(__dirname, './dist'),
+    assets: 'assets/'
+}
+
 module.exports = {
-    entry:{
-        app: './src/index.js'
+    externals: {
+        paths: PATHS
     },
-    output:{
-        filename: '[name].js',
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist'
+    entry: {
+        app: PATHS.src
+    },
+    output: {
+        filename: `${PATHS.assets}js/[name].js`,
+        path: PATHS.dist,
+        publicPath: '/'
     },
     module: {
         rules: [
-        {
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: '/node_modules/'
-        },
-        {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                MiniCssExtractPlugin.loader, 
-                'css-loader',
-                'postcss-loader'
-            ]
-        },
-        {
-            test: /\.scss$/,
-            use: [
-                'style-loader',
-                MiniCssExtractPlugin.loader, 
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
     },
     plugins: [
         new MiniCssExtractPlugin({
-          filename: '[name].css'
+            filename: `${PATHS.assets}css/[name].css`
         })
     ]
 }
